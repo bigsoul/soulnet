b77c72f4fc61a0deae5726ef3857a3320ca5b9ed
Vitaliy Vaskovich <vvv93rus@gmail.com>
2021-08-25 04:28:57 +0300

    Add player behavior

--- frontend/src/components/Player.tsx
+++ frontend/src/components/Player.tsx
@@ -1,7 +1,12 @@
+import { PureComponent } from "react";
 import styled from "styled-components";
 import Button from "./Button";
 
+import EPlayerState from "../enums/EPlayerState";
+
+import playerStart from "../assets/svg/player-start.svg";
 import playerPlay from "../assets/svg/player-play.svg";
+import playerPause from "../assets/svg/player-pause.svg";
 import playerStop from "../assets/svg/player-stop.svg";
 
 const PlayerDiv = styled.div`
@@ -13,25 +18,73 @@ const PlayerDiv = styled.div`
 	margin-right: 10px;
 `;
 
-const Player = () => {
-	return (
-		<PlayerDiv>
-			<Button
-				template="icon"
-				svgPath={playerPlay}
-				onClick={() => {
-					console.log("click: playerPlay");
-				}}
-			/>
-			<Button
-				template="icon"
-				svgPath={playerStop}
-				onClick={() => {
-					console.log("click: playerStop");
-				}}
-			/>
-		</PlayerDiv>
-	);
-};
+interface IPlayerProps {
+	state: EPlayerState;
+}
+
+class Player extends PureComponent<Partial<IPlayerProps>, IPlayerProps> {
+	constructor(props: Partial<IPlayerProps>) {
+		super(props);
+		this.state = {
+			state: EPlayerState.Config,
+		};
+	}
+
+	static getDerivedStateFromProps = (
+		props: Partial<IPlayerProps>,
+		state: IPlayerProps
+	): IPlayerProps => {
+		return {
+			state: props.state ? props.state : state.state,
+		};
+	};
+
+	hendlerButtonLeft = () => {
+		const { state } = this.state;
+
+		if (state === EPlayerState.Config || state === EPlayerState.Paused) {
+			this.setState({ state: EPlayerState.Playing });
+		} else if (state === EPlayerState.Playing) {
+			this.setState({ state: EPlayerState.Paused });
+		}
+	};
+
+	hendlerButtonRight = () => {
+		this.setState({ state: EPlayerState.Config });
+	};
+
+	render = () => {
+		const { state } = this.state;
+
+		let buttonLeft = playerStart;
+		let buttonStop = playerStop;
+
+		if (state === EPlayerState.Playing) {
+			buttonLeft = playerPause;
+			buttonStop = playerStop;
+		} else if (state === EPlayerState.Paused) {
+			buttonLeft = playerPlay;
+			buttonStop = playerStop;
+		} else if (state === EPlayerState.Completed) {
+			buttonLeft = playerStart;
+			buttonStop = playerStop;
+		}
+
+		return (
+			<PlayerDiv>
+				<Button
+					template="icon"
+					svgPath={buttonLeft}
+					onClick={this.hendlerButtonLeft}
+				/>
+				<Button
+					template="icon"
+					svgPath={buttonStop}
+					onClick={this.hendlerButtonRight}
+				/>
+			</PlayerDiv>
+		);
+	};
+}
 
 export default Player;
---
+++ frontend/src/enums/EPlayerState.ts
@@ -0,0 +1,8 @@
+enum EPlayerState {
+	Config = 0,
+	Playing = 1,
+	Paused = 2,
+	Completed = 3,
+}
+
+export default EPlayerState;
